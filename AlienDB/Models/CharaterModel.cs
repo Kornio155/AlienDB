addb316b1306fb054e79456a96902c2e65f1aa16
kornel <gg@g.k>
2025-11-13 10:20:47 +0100

    Dodanie plików modeli postaci i filmow

---
+++ AlienDB/Models/CharaterModel.cs
@@ -0,0 +1,15 @@
+namespace AlienDB.Models;
+
+public class CharaterModel
+{
+    public string Name { get; set; } = "";
+    public string Film { get; set; } = "";
+    public string Role  { get; set; } = "";
+    public string Actor  { get; set; } = "";
+    public string Race  { get; set; } = "";
+    public int BirthYear { get; set; } = 0000;
+    public string FunctionCrew { get; set; } = "";
+    public string Profile { get; set; } = "";
+    public string Fate  { get; set; } = "";
+    public string FunFact { get; set; } = "";
+}
---
+++ AlienDB/Models/FilmModel.cs
@@ -0,0 +1,18 @@
+namespace AlienDB.Models;
+
+public class AlienModel
+{
+    public string Title { get; set; } = "";
+    public string PolishTitle { get; set; } = "";
+    public int Year { get; set; } = 0000;
+    public string Director { get; set; } = "";
+    public string Scenario { get; set; } = "";
+    public string Genre { get; set; } = "";
+    public string Duration { get; set; } = "";
+    public double Rating { get; set; } = 0;
+    public string MainCharacters { get; set; } = "";
+    public string Ship { get; set; } = "";
+    public string Description { get; set; } = "";
+    public string FunFuct { get; set; } = "";
+    
+}
